df00ecc933178637ed05f762f76fdc3b2ee01001
xprom115-rgb <xprom115@gmail.com>
2026-04-01 18:33:37 +0200

    Create  firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDuY6GIkbxmgIQne4AwWwEYinC_oz2umyw",
+  authDomain: "kuzelky-benesov.firebaseapp.com",
+  projectId: "kuzelky-benesov",
+  storageBucket: "kuzelky-benesov.firebasestorage.app",
+  messagingSenderId: "370227845074",
+  appId: "1:370227845074:web:f1f6c5ed1ae3bde1395a0b"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db  = getFirestore(app);
+
+console.log("firebase-config.js načten – Firestore inicializován");
