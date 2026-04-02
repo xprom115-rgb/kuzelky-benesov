9dc087004948f57c2ef045e5e0074e9cab40bfe2
xprom115-rgb <xprom115@gmail.com>
2026-04-02 10:53:35 +0200

    Create admin-config.js

---
+++ admin-config.js
@@ -0,0 +1,4 @@
+// admin-config.js
+// SHA-256("xprom115") = ff3d4cf3d9f015683dec9a2f026766f5cc3f75bee52cb54fb9cf602e3a571578
+export const ADMIN_PASSWORD_HASH =
+  "ff3d4cf3d9f015683dec9a2f026766f5cc3f75bee52cb54fb9cf602e3a571578";
