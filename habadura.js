764c9a3f0eb1696e8d5b0d6711632a2639e16f77
xprom115-rgb <xprom115@gmail.com>
2026-04-07 14:14:58 +0200

    Update habadura.js

--- habadura.js
+++ habadura.js
@@ -41,11 +41,6 @@ let SEASON_ID = null;
 let PHASE = null;
 let seasonReady = false;
 
-
-// ===== AUTO sezóna/fáze z Firestore (seasons) =====
-let SEASON_ID = null;
-let PHASE = null;
-
 // pro jistotu: dokud nevíme sezónu/fázi, nenecháme ukládat zápas
 let seasonReady = false;
 
