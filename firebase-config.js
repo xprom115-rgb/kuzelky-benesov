10dc76f53c8b190e8aa63d1c830ffc696acb8456
xprom115-rgb <xprom115@gmail.com>
2026-04-20 19:50:22 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,9 @@
-// firebase-config.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
+
 export const firebaseConfig = {
   apiKey: "AIzaSyDuY6GIkbxmgIQne4AwWwEYinC_oz2umyw",
   authDomain: "kuzelky-benesov.firebaseapp.com",
@@ -10,9 +12,10 @@ export const firebaseConfig = {
   messagingSenderId: "370227845074",
   appId: "1:370227845074:web:f1f6c5ed1ae3bde1395a0b"
 };
-
 export const app = initializeApp(firebaseConfig);
 export const db  = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
+
 console.log("firebase-config.js načten – Firestore inicializován");
-``
+
