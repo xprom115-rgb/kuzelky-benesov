9ab817b9b0850b181f51731212f4ed70d5669d1b
xprom115-rgb <xprom115@gmail.com>
2026-04-20 18:42:52 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 export const firebaseConfig = {
   apiKey: "AIzaSyDuY6GIkbxmgIQne4AwWwEYinC_oz2umyw",
   authDomain: "kuzelky-benesov.firebaseapp.com",
@@ -13,6 +13,6 @@ export const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db  = getFirestore(app);
-
+export const auth = getAuth(app);
 console.log("firebase-config.js načten – Firestore inicializován");
 ``
